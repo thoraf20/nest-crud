feef72aa0b2bbe0e713207ada78e635970b35560
Toheeb Rauf <50268671+thoraf20@users.noreply.github.com>
2021-03-27 11:07:56 -1100

    basic crud operation

---
+++ src/books/dto/create-book.dto.ts
@@ -0,0 +1,7 @@
+/* eslint-disable prettier/prettier */
+export class CreateBookDTO {
+  readonly id: number;
+  readonly title: string;
+  readonly description: string;
+  readonly author: string;
+}
